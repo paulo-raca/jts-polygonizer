b539049148813fa94ef02c83e4e32c2fa66c9c7e
Paulo Costa <paulo.costa@geofusion.com.br>
2016-05-13 14:19:27 -0300

    First version

---
+++ src/Polygonizer.java
@@ -0,0 +1,338 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+import com.google.common.base.Function;
+import com.google.common.base.Predicate;
+import com.google.common.base.Stopwatch;
+import com.google.common.collect.Iterators;
+import com.google.common.collect.Lists;
+import com.vividsolutions.jts.algorithm.CGAlgorithms;
+import com.vividsolutions.jts.geom.Coordinate;
+import com.vividsolutions.jts.geom.Geometry;
+import com.vividsolutions.jts.geom.GeometryFactory;
+import com.vividsolutions.jts.geom.LineString;
+import com.vividsolutions.jts.geom.LinearRing;
+import com.vividsolutions.jts.geom.MultiLineString;
+import com.vividsolutions.jts.geom.MultiPolygon;
+import com.vividsolutions.jts.geom.Polygon;
+import com.vividsolutions.jts.geom.util.PolygonExtracter;
+import com.vividsolutions.jts.io.WKTReader;
+
+public class Polygonizer implements Iterable<Polygon> {
+    private static final GeometryFactory GEOMETRY_FACTORY = new GeometryFactory();
+
+    Map<Coordinate, Vertex> vertexes;
+    double streetBuffer;
+    double outerBuffer;
+    double innerBuffer;
+
+    public Polygonizer(List<LineString> segments, double streetBuffer, double outerBuffer, double innerBuffer) {
+        this.vertexes = new HashMap<>();
+        this.streetBuffer = streetBuffer;
+        this.outerBuffer = outerBuffer;
+        this.innerBuffer = innerBuffer;
+
+        //Create a graph
+        for (LineString segment : segments) {
+            Coordinate c1 = segment.getCoordinateN(0);
+            Coordinate c2 = segment.getCoordinateN(segment.getNumPoints()-1);
+
+            Vertex v1 = vertexes.get(c1);
+            if (v1 == null) {
+                v1 = new Vertex(c1);
+                vertexes.put(c1, v1);
+            }
+            Vertex v2 = vertexes.get(c2);
+            if (v2 == null) {
+                v2 = new Vertex(c2);
+                vertexes.put(c2, v2);
+            }
+
+            Edge edge = new Edge(v1, v2, segment);
+
+            v1.edges.add(edge);
+            v2.edges.add(edge.reverse);            
+        }
+
+        //Sort edges of every vertex
+        for (Vertex v : vertexes.values()) {
+            Collections.sort(v.edges);
+            for (int i=0; i<v.edges.size(); i++) {
+                Edge e = v.edges.get(i);
+                e.index_v1 = i;
+                e.reverse.index_v2 = i;
+            }
+        }
+    }
+
+    @Override
+    public Iterator<Polygon> iterator() {
+        //Clear visited flags
+        for (Vertex v : vertexes.values()) {
+            for (Edge e : v.edges) {
+                e.visited = false;
+            }
+        }
+
+        Iterator<Vertex> vertexesIt = vertexes.values().iterator();
+        Iterator<Edge> edgeIt = Iterators.concat(Iterators.transform(vertexesIt, new Function<Vertex, Iterator<Edge>>() {
+            @Override
+            public Iterator<Edge> apply(Vertex v) {
+                return v.edges.iterator();
+            }
+        }));
+        Iterator<Edge> unvisitedEdgeIt = Iterators.filter(edgeIt, new Predicate<Edge>() {
+            @Override
+            public boolean apply(Edge edge) {
+                return !edge.visited;
+            }
+        });
+        Iterator<Polygon> polygonIt = Iterators.concat(Iterators.transform(unvisitedEdgeIt, new Function<Edge, Iterator<Polygon>>() {
+            @Override
+            @SuppressWarnings("unchecked")
+            public Iterator<Polygon> apply(Edge e) {
+                return PolygonExtracter.getPolygons(visit(e)).iterator();
+            }
+        }));
+        Iterator<Polygon> nonEmptyPolygonIt = Iterators.filter(polygonIt, new Predicate<Polygon>() {
+            @Override
+            public boolean apply(Polygon polygon) {
+                return !polygon.isEmpty();
+            }
+        });
+        return nonEmptyPolygonIt;
+    } 
+
+    public List<Geometry> get() {
+        return Lists.newArrayList(iterator());
+    }
+
+    private Geometry visit(Edge firstEdge) {
+        LinkedList<Edge> ring = new LinkedList<>();
+        List<Edge> deadEnds = new ArrayList<>();
+        Edge currentEdge = firstEdge;
+
+        while (true) {
+            Vertex currentVertex = currentEdge.v2;
+            currentEdge = currentVertex.edges.get((currentEdge.index_v2 + 1) % currentVertex.edges.size());
+            if (currentEdge.visited) {
+                throw new IllegalStateException("Edge visited twice?! " + currentEdge);
+            }
+            currentEdge.visited = true;
+
+            if (!ring.isEmpty() && ring.getLast().v1 == currentEdge.v2) {
+                ring.removeLast();
+                deadEnds.add(currentEdge);
+            } else {
+                ring.add(currentEdge);
+            }
+
+            if (currentEdge == firstEdge) {
+                break;
+            }
+        }
+
+        while (!ring.isEmpty() && ring.getFirst().v2 == ring.getLast().v1) {
+            deadEnds.add(ring.removeFirst());
+            ring.removeLast();
+        }
+
+
+        Polygon polygon = polygonFromEdges(ring);
+        MultiLineString deadEndGeom = geometryFromDeadEnds(deadEnds);
+
+        Geometry ret = polygon;
+        if (CGAlgorithms.signedArea(polygon.getExteriorRing().getCoordinates()) > 0) {
+            if (streetBuffer != 0) {
+                ret = ret.buffer(-streetBuffer);
+            }
+            if (streetBuffer > 0) {
+                ret = ret.difference(deadEndGeom.buffer(streetBuffer));
+            }
+
+            if (innerBuffer > 0) {
+                ret = ret.difference(ret.buffer(-innerBuffer));
+            }
+        } else {
+            if (outerBuffer > 0) {
+                Geometry outer = ret.buffer(outerBuffer);
+                outer = outer.union(deadEndGeom.buffer(outerBuffer));
+
+                Geometry inner = ret;
+                if (streetBuffer != 0) {
+                    inner = inner.buffer(streetBuffer);
+                }
+                if (streetBuffer > 0) {
+                    inner = inner.union(deadEndGeom.buffer(streetBuffer));
+                }
+                ret = outer.difference(inner);
+            } else {
+                ret = GEOMETRY_FACTORY.createPolygon(null, null);
+            }
+        }
+        if (!ret.isEmpty()) {
+            //ret = TopologyPreservingSimplifier.simplify(ret,  0.0001);
+            //System.out.println(ret);
+        }
+        return ret;
+    }
+
+
+    private Polygon polygonFromEdges(List<Edge> ring) {
+        if (ring.isEmpty()) {
+            return GEOMETRY_FACTORY.createPolygon(null, null); 
+        }
+
+        List<Coordinate> coords = new ArrayList<>();
+        for (Edge edge : ring) {
+            for (int i=0; i<edge.path.getNumPoints()-1; i++) {
+                coords.add(edge.path.getCoordinateN(i));
+            }
+        }
+        coords.add(coords.get(0));
+        LinearRing linearRing = GEOMETRY_FACTORY.createLinearRing(coords.toArray(new Coordinate[coords.size()]));
+        return GEOMETRY_FACTORY.createPolygon(linearRing, new LinearRing[0]);
+    }
+
+    private MultiLineString geometryFromDeadEnds(List<Edge> deadEnds) {
+        List<LineString> segments = new ArrayList<>();
+        for (Edge edge : deadEnds) {
+            segments.add(edge.path);
+        }
+        return GEOMETRY_FACTORY.createMultiLineString(segments.toArray(new LineString[segments.size()]));
+    }
+
+
+    private static class Vertex {
+        Coordinate coord;
+        List<Edge> edges;
+
+        public Vertex(Coordinate center) { 
+            this.coord = center;
+            this.edges = new ArrayList<>();
+        }
+        @Override
+        public String toString() {
+            return "(" + coord.x + "," + coord.y + ")";
+        }
+    }
+
+    private static class Edge implements Comparable<Edge>{
+        Vertex v1;
+        Vertex v2;
+        int index_v1, index_v2;
+        final double sortValue;
+        final LineString path;
+        final Edge reverse;
+        boolean visited;
+
+        public Edge(Vertex v1, Vertex v2, LineString path) {
+            this(v1, v2, path, null);
+        }
+
+        private Edge(Vertex v1, Vertex v2, LineString path, Edge reverse) {
+            this.v1 = v1;
+            this.v2 = v2;
+            this.path = path;
+            this.visited = false;
+            this.reverse = reverse != null ? reverse : new Edge(v2, v1, path.reverse(), this);
+
+            Coordinate c1 = path.getCoordinateN(0);
+            Coordinate c2 = path.getCoordinateN(1);
+            this.sortValue = Math.atan2(c2.y-c1.y, c2.x-c1.x);
+        }
+
+        @Override
+        public int compareTo(Edge o) {
+            return Double.compare(this.sortValue, o.sortValue);
+        }
+
+        @Override
+        public String toString() {
+            return v1 + "→" + v2;
+        }
+    }
+
+
+    private static double meters(double meters) {
+        return Math.toDegrees(meters / 6378137.0);
+    }
+
+    public static void main(String[] args) throws Exception {
+        /*
+        Coordinate A = new Coordinate(0, 0);
+        Coordinate B = new Coordinate(0, 2);
+        Coordinate C = new Coordinate(2, 3);
+        Coordinate D = new Coordinate(3, 1);
+        Coordinate E = new Coordinate(2, 1);
+        Coordinate F = new Coordinate(1, 2);
+        Coordinate G = new Coordinate(4, 3);
+        Coordinate H = new Coordinate(3, 2);
+        Coordinate I = new Coordinate(3, 4);
+        Coordinate J = new Coordinate(4, 5);
+
+        LineString[] segments = {
+                GEOMETRY_FACTORY.createLineString(new Coordinate[]{A, B}),
+                GEOMETRY_FACTORY.createLineString(new Coordinate[]{A, D}),
+                GEOMETRY_FACTORY.createLineString(new Coordinate[]{A, E}),
+
+                GEOMETRY_FACTORY.createLineString(new Coordinate[]{B, C}),
+                GEOMETRY_FACTORY.createLineString(new Coordinate[]{B, F}),
+
+                GEOMETRY_FACTORY.createLineString(new Coordinate[]{C, D}),
+                GEOMETRY_FACTORY.createLineString(new Coordinate[]{C, F}),
+                GEOMETRY_FACTORY.createLineString(new Coordinate[]{C, H}),
+
+                GEOMETRY_FACTORY.createLineString(new Coordinate[]{D, G}),
+
+                GEOMETRY_FACTORY.createLineString(new Coordinate[]{E, F}),
+
+                GEOMETRY_FACTORY.createLineString(new Coordinate[]{G, H}),
+
+                GEOMETRY_FACTORY.createLineString(new Coordinate[]{H, I}),
+
+                GEOMETRY_FACTORY.createLineString(new Coordinate[]{I, J})
+        };
+        System.out.println(GEOMETRY_FACTORY.createMultiLineString(segments));
+
+        List<Geometry> polygons = new Poligonizer(Arrays.asList(segments), -0.04, 1).get();
+        System.out.println(GEOMETRY_FACTORY.createMultiPolygon(polygons.toArray(new Polygon[polygons.size()])));
+         */
+
+        //BufferedReader in = new BufferedReader(new InputStreamReader(Poligonizer.class.getResourceAsStream("street_segments.wkt"), "UTF-8"));
+        List<LineString> segments = new ArrayList<>();
+        try (BufferedReader in = new BufferedReader(new FileReader("src/street_segments.wkt"))) {
+
+            while (in.ready()) {
+                String line = in.readLine();
+                segments.add((LineString)new WKTReader().read(line));
+            }
+        }
+        System.out.println(GEOMETRY_FACTORY.createMultiLineString(segments.toArray(new LineString[segments.size()])));
+
+        System.out.println("Data read: " + segments.size() + " segments");
+        Stopwatch timer = Stopwatch.createStarted();
+
+        Polygonizer poligonizer = new Polygonizer(segments, 0, 0, 0);
+        //Poligonizer poligonizer = new Poligonizer(segments, meters(2), meters(50), 0);
+        //Poligonizer poligonizer = new Poligonizer(segments, meters(10), 0, 0);
+        //Poligonizer poligonizer = new Poligonizer(segments, -meters(10), 0, 0);
+        //Poligonizer poligonizer = new Poligonizer(segments, meters(2), 0, meters(50));
+        //Poligonizer poligonizer = new Poligonizer(segments, 0.0001, 0.001, 0.0005);
+        System.out.println("Graph prepared - " + timer);
+
+        List<Geometry> polygons = poligonizer.get();
+        System.out.println(polygons.size() + " polygons found - " + timer);
+
+        MultiPolygon allPolygons = GEOMETRY_FACTORY.createMultiPolygon(polygons.toArray(new Polygon[polygons.size()]));
+        //System.out.println(allPolygons);
+        //System.out.println(TopologyPreservingSimplifier.simplify(allPolygons, meters(10)));
+    }
+}
---
+++ src/RangeMultiMap.java
@@ -0,0 +1,265 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.SortedMap;
+import java.util.TreeMap;
+
+import com.google.common.base.Function;
+import com.google.common.collect.AbstractIterator;
+import com.google.common.collect.BoundType;
+import com.google.common.collect.Iterables;
+import com.google.common.collect.Iterators;
+import com.google.common.collect.Range;
+
+public class RangeMultiMap<K extends Comparable<K>, V> {
+    Comparator<Range<K>> rangeComparator = new RangeComparator<>();
+    DomainOperation<K, K> domainOperations;
+    Map<K, SortedMap<Range<K>, Collection<V>>> scaleMapss = new HashMap<>();
+
+    public RangeMultiMap(DomainOperation<K,K> domainOperations) {
+        this.domainOperations = domainOperations;
+    }
+    
+    public boolean put(Range<K> range, V value) {
+        K scaleLength = domainOperations.discreteLen(range);
+        SortedMap<Range<K>, Collection<V>> scaleData = scaleMapss.get(scaleLength);
+        if (scaleData == null) {
+            scaleData = new TreeMap<>(rangeComparator);
+            scaleMapss.put(scaleLength, scaleData);
+        }
+        Collection<V> values = scaleData.get(range);
+        if (values == null) {
+            values = new ArrayList<>();
+            scaleData.put(range, values);
+        }
+        return values.add(value);
+    }
+
+
+    public boolean remove(Range<K> range, V value) {
+        K scaleLength = domainOperations.discreteLen(range);
+        SortedMap<Range<K>, Collection<V>> scaleData = scaleMapss.get(scaleLength);
+        if (scaleData == null) {
+            return false;
+        }
+        Collection<V> values = scaleData.get(range);
+        if (values == null) {
+            return false;
+        }
+        if (!values.remove(value)) {
+            return false;
+        }
+        if (values.isEmpty()) {
+            scaleData.remove(range);
+            if (scaleData.isEmpty()) {
+                scaleMapss.remove(scaleLength);
+            }
+        }
+        return true;
+    }
+
+    public Iterable<Range<K>> getKeys(Range<K> query, RangeSearchType searchType) {
+        return Iterables.transform(getEntries(query, searchType), new Function<Map.Entry<Range<K>, Collection<V>>, Range<K>>() {
+            public Range<K> apply(Map.Entry<Range<K>, Collection<V>> entry) {
+                return entry.getKey();
+            };            
+        });
+    }
+
+    public Iterable<V> getValues(Range<K> query, RangeSearchType searchType) {
+        return Iterables.concat(Iterables.transform(getEntries(query, searchType), new Function<Map.Entry<Range<K>, Collection<V>>, Collection<V>>() {
+            public Collection<V> apply(Map.Entry<Range<K>, Collection<V>> entry) {
+                return entry.getValue();
+            };
+        }));
+    }
+
+    public Iterable<Map.Entry<Range<K>, Collection<V>>> getEntries(final Range<K> query, final RangeSearchType rangeSearchType) {
+        return new Iterable<Map.Entry<Range<K>, Collection<V>>>() {
+            @Override
+            public Iterator<Entry<Range<K>, Collection<V>>> iterator() {
+                Collection<Iterator<Map.Entry<Range<K>, Collection<V>>>> scaleIterators = new ArrayList<>();
+                for (Map.Entry<K, SortedMap<Range<K>, Collection<V>>> entry : scaleMapss.entrySet()) {
+                    K scale = entry.getKey();
+                    SortedMap<Range<K>, Collection<V>> scaleValues = entry.getValue();
+
+                    if (query.hasLowerBound() && scale != null) {
+                        //TODO: Slicing for INTERSECTS, could be more optimized for other operations
+                        K minValue = domainOperations.sub(query.lowerEndpoint(), scale);
+                        scaleValues = scaleValues.tailMap( Range.closedOpen(minValue, minValue ) );
+                    }
+
+                    final Iterator<Entry<Range<K>, Collection<V>>> preFilterIterator = scaleValues.entrySet().iterator();
+
+                    scaleIterators.add(new AbstractIterator<Entry<Range<K>, Collection<V>>>() {
+                        @Override
+                        protected Entry<Range<K>, Collection<V>> computeNext() {
+                            while (true) {
+                                if (!preFilterIterator.hasNext()) {
+                                    return endOfData();
+                                }
+
+                                Entry<Range<K>, Collection<V>> entry = preFilterIterator.next();
+
+                                //TODO: Slicing for INTERSECTS, could be more optimized for other operations
+                                if (query.hasUpperBound() && entry.getKey().hasLowerBound() && entry.getKey().lowerEndpoint().compareTo(query.upperEndpoint()) > 0) {
+                                    return endOfData();
+                                }
+
+                                if (rangeSearchType.predicate(query, entry.getKey())) {
+                                    return entry;
+                                }
+                            }
+                        }
+                    });
+                }
+                return Iterators.mergeSorted(scaleIterators, new Comparator<Map.Entry<Range<K>, Collection<V>>>() {
+                    @Override
+                    public int compare(Entry<Range<K>, Collection<V>> o1, Entry<Range<K>, Collection<V>> o2) {
+                        return rangeComparator.compare(o1.getKey(), o2.getKey());
+                    }
+                });
+            }
+        };
+    }
+
+    public static enum RangeSearchType {
+        INTERSECTS {
+            @Override
+            public <V extends Comparable<V>> boolean predicate(Range<V> query, Range<V> value) {
+                return query.isConnected(value) && !query.intersection(value).isEmpty();
+            }
+        },
+        ENCLOSES {
+            @Override
+            public <V extends Comparable<V>> boolean predicate(Range<V> query, Range<V> value) {
+                return query.encloses(value);
+            }
+        }, 
+        ENCLOSED {
+            @Override
+            public <V extends Comparable<V>> boolean predicate(Range<V> query, Range<V> value) {
+                return value.encloses(query);
+            }
+        };
+
+        public abstract <V extends Comparable<V>> boolean predicate(Range<V> query, Range<V> value);
+    }
+
+
+    public static interface DomainOperation<V extends Comparable<V>, L> {
+        public L len(Range<V> range);
+        public L discreteLen(Range<V> range);
+        public V sub(V a, L b);
+
+        public static final DomainOperation<Double, Double> DOUBLE = new DomainOperation<Double, Double> () {
+            public Double len(Range<Double> range) {
+                if (range.hasLowerBound() && range.hasUpperBound()) {
+                    return range.upperEndpoint() - range.lowerEndpoint();
+                } else {
+                    return null;
+                }
+            }
+            public Double discreteLen(Range<Double> range) {
+                Double len = len(range);
+                if (len == null || len == 0) {
+                    return len;
+                }
+                return Math.exp(Math.ceil(Math.log(len)));
+            }
+            public Double sub(Double a, Double b) {
+                return a-b;
+            }
+        };
+
+
+        public static final DomainOperation<Long, Long> LONG = new DomainOperation<Long, Long> () {
+            public Long len(Range<Long> range) {
+                if (range.hasLowerBound() && range.hasUpperBound()) {
+                    return range.upperEndpoint() - range.lowerEndpoint();
+                } else {
+                    return null;
+                }
+            }
+            public Long discreteLen(Range<Long> range) {
+                Long len = len(range);
+                if (len == null || len == 0) {
+                    return len;
+                }
+                long ret = 0;
+                while (ret < len) {
+                    ret = ret*2 + 1;
+                }
+                return ret;
+            }
+            public Long sub(Long a, Long b) {
+                return a-b;
+            }
+        };
+    }
+
+
+    public static class RangeComparator<K extends Comparable<K>> implements Comparator<Range<K>> {
+        @Override
+        public int compare(Range<K> o1, Range<K> o2) {
+            //Ranges without lower bound
+            if (o1.hasLowerBound() != o2.hasLowerBound()) {
+                return !o1.hasLowerBound() ? -1 : 1;
+            }
+            //Ranges with lowest lower bound
+            if (o1.hasLowerBound() && o2.hasLowerBound()) {
+                int ret = o1.lowerEndpoint().compareTo(o2.lowerEndpoint());
+                if (ret != 0) return ret;
+                if (o1.lowerBoundType() != o2.lowerBoundType()) {
+                    return o1.lowerBoundType() == BoundType.CLOSED ? -1 : 1;
+                }
+            }
+
+
+            //Ranges with upper bound
+            if (o1.hasUpperBound() != o2.hasUpperBound()) {
+                return o1.hasUpperBound() ? -1 : 1;
+            }
+            //Ranges with lowest upper bound
+            if (o1.hasUpperBound() && o2.hasUpperBound()) {
+                int ret = o1.upperEndpoint().compareTo(o2.upperEndpoint());
+                if (ret != 0) return ret;
+                if (o1.lowerBoundType() != o2.lowerBoundType()) {
+                    return o1.lowerBoundType() == BoundType.OPEN? -1 : 1;
+                }
+            }
+
+
+            return 0;
+        }
+    }
+    
+    public static void main(String[] args) {
+        RangeMultiMap<Long, Object> rmm = new RangeMultiMap<>(DomainOperation.LONG);
+        rmm.put(Range.all(), "ALL");
+        rmm.put(Range.atLeast(10L), ">= 10");
+        rmm.put(Range.atLeast(20L), ">= 20");
+        rmm.put(Range.atLeast(30L), ">= 30");
+        rmm.put(Range.atMost(10L), "<= 10");
+        rmm.put(Range.atMost(20L), "<= 20");
+        rmm.put(Range.atMost(30L), "<= 30");
+        rmm.put(Range.closed(10L, 30L), "[10..30]");
+        rmm.put(Range.closed(15L, 25L), "[15..25]");
+        rmm.put(Range.closed(5L, 10L), "[5..10]");
+        rmm.put(Range.closed(10L, 15L), "[10..15]");
+        rmm.put(Range.singleton(5L), "[5]");
+        rmm.put(Range.singleton(7L), "[7]");
+        
+        System.out.println(Iterables.toString(rmm.getValues(Range.greaterThan(20L), RangeSearchType.INTERSECTS)));
+        System.out.println(Iterables.toString(rmm.getValues(Range.singleton(7L), RangeSearchType.ENCLOSED)));
+        System.out.println(Iterables.toString(rmm.getValues(Range.singleton(7L), RangeSearchType.ENCLOSES)));
+        System.out.println(Iterables.toString(rmm.getValues(Range.singleton(7L), RangeSearchType.INTERSECTS)));
+        
+        System.out.println(Iterables.toString(rmm.getValues(Range.closed(7L, 27L), RangeSearchType.INTERSECTS)));
+        
+    }
+}
